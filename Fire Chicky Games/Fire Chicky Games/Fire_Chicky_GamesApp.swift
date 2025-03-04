458201d608483a36e8c0dbfff187d73a8e6bdb0c
Dias <dias.atudinov@gmail.com>
2025-03-04 18:06:29 +0500

    first commit

---
+++ Fire Chicky Games/Fire Chicky Games/Fire_Chicky_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fire_Chicky_GamesApp.swift
+//  Fire Chicky Games
+//
+//  Created by Dias Atudinov on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Fire_Chicky_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
