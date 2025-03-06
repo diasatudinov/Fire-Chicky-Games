ae42759ba817f16045f8b32e4476250e4c85ac13
Dias <dias.atudinov@gmail.com>
2025-03-06 19:22:48 +0500

    add Game View

--- Fire Chicky Games/Fire Chicky Games/FCModules/FCGameView/FCTrapSweeperGameView.swift
+++ Fire Chicky Games/Fire Chicky Games/FCModules/FCGameView/FCTrapSweeperGameView.swift
@@ -8,18 +8,155 @@
 import SwiftUI
 
 struct FCTrapSweeperGameView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State var openLevel = false
+    @State var currentLevel: Level?
+    @ObservedObject var viewModel = FCTrapSweeperGameViewModel()
+    
+    @State private var currentPage: Int = 0
+    let levelsPerPage = 10
+    
     var body: some View {
         // Пример уровня, задаваемого в коде
-        let level1 = Level(
-            rows: 5,
-            columns: 5,
-            flames: [(row: 1, col: 2), (row: 2, col: 1), (row: 3, col: 3)],
-            arrows: [((row: 2, col: 3), .left), ((row: 3, col: 0), .up)],
-            nest: (row: 4, col: 4),
-            chickenStart: (row: 0, col: 0)
+        ZStack {
+            VStack {
+                // Determine the range for the current page
+                let startIndex = currentPage * levelsPerPage
+                let endIndex = min(startIndex + levelsPerPage, viewModel.levels.count)
+                let currentLevels = Array(viewModel.levels[startIndex..<endIndex])
+                
+                
+                HStack {
+                    if currentPage > 0 {
+                        Button(action: {
+                            withAnimation {
+                                currentPage -= 1
+                            }
+                        }) {
+                            Image(.levelArrow)
+                                .resizable()
+                                .scaledToFit()
+                                .rotationEffect(Angle(degrees: 180))
+                                .frame(height: 80)
+                        }
+                    } else {
+                        Image(.levelArrow)
+                            .resizable()
+                            .scaledToFit()
+                            .rotationEffect(Angle(degrees: 180))
+                            .frame(height: 80)
+                            .opacity(0)
+                    }
+                    
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 5), spacing: 10) {
+                        ForEach(0..<currentLevels.count, id: \.self) { index in
+                            let levelNumber = startIndex + index + 1
+                            Button(action: {
+                                    DispatchQueue.main.async {
+                                        currentLevel = viewModel.levels[startIndex + index]
+                                   }
+                                    
+                                    openLevel = true
+                            }) {
+                                ZStack {
+                                    Image(viewModel.levels[startIndex + index].levelPass ? .yellowCellFC : .blueCellFC)
+                                        .resizable()
+                                        .scaledToFit()
+                                    Text("\(levelNumber)")
+                                        .font(.system(size: 60, weight: .black))
+                                        .foregroundColor(.white)
+                                        .cornerRadius(8)
+                                }.frame(width: 100, height: 100)
+                            }.disabled(!viewModel.levels[startIndex + index].levelPass)
+                        }
+                    }.frame(width: 550)
+                    
+                    
+                    if (currentPage + 1) * levelsPerPage < viewModel.levels.count {
+                        Button(action: {
+                            withAnimation {
+                                currentPage += 1
+                            }
+                        }) {
+                            Image(.levelArrow)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 80)
+                        }
+                    } else {
+                        Image(.levelArrow)
+                            .resizable()
+                            .scaledToFit()
+                            .rotationEffect(Angle(degrees: 180))
+                            .frame(height: 80)
+                            .opacity(0)
+                    }
+                    
+                }.padding(.horizontal)
+//                ForEach(Range(0...viewModel.levels.count - 1)) { index in
+//                    Text("\(index + 1)")
+//                        .padding()
+//                        .background(.white)
+//                    
+//                        .onTapGesture {
+//                            DispatchQueue.main.async {
+//                                currentLevel = viewModel.levels[index]
+//                           }
+//                            
+//                           
+//                            openLevel = true
+//                            
+//                        }
+//                }
+            }
+            
+            VStack {
+                HStack {
+                    
+                    Button {
+                       presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(.backIconFC)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 60)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }.padding()
+        }
+        .background(
+            Image(.bgFC)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+                .blur(radius: 4)
+            
         )
-        
-        GameView(game: GameModel(level: level1))
+        .fullScreenCover(isPresented: $openLevel) {
+            if let level = currentLevel {
+                GameView(game: GameModel(level: level), backBtnHandle: {
+                        currentLevel = nil
+                }, gameWonHandle: {
+                    viewModel.levelUp(index: level.num)
+                })
+            } else {
+                VStack {
+                    HStack {
+                        Spacer()
+                    }
+                    Spacer()
+                }.background(
+                    Image(.bgFC)
+                        .resizable()
+                        .ignoresSafeArea()
+                        .scaledToFill()
+                        .blur(radius: 4)
+                    
+                )
+            }
+        }
     }
 }
 
--- Fire Chicky Games/Fire Chicky Games/FCModules/FCGameView/FCTrapSweeperGameViewModel.swift
+++ Fire Chicky Games/Fire Chicky Games/FCModules/FCGameView/FCTrapSweeperGameViewModel.swift
@@ -6,3 +6,165 @@
 //
 
 import Foundation
+
+class FCTrapSweeperGameViewModel: ObservableObject {
+    @Published var levels: [Level] = [
+        //Level 1
+        Level(
+            num: 1, rows: 2,
+            columns: 3,
+            flames: [(row: 0, col: 1)],
+            arrows: [],
+            nest: (row: 0, col: 2),
+            chickenStart: (row: 0, col: 0),
+            levelPass: true
+        ),
+        
+        //Level 2
+        Level(
+            num: 2, rows: 3,
+            columns: 2,
+            flames: [(row: 1, col: 0)],
+            arrows: [],
+            nest: (row: 2, col: 0),
+            chickenStart: (row: 0, col: 0)
+        ),
+        
+        //Level 3
+        Level(
+            num: 3, rows: 3,
+            columns: 3,
+            flames: [(row: 1, col: 0), (row: 2, col: 0), (row: 1, col: 1), (row: 2, col: 1)],
+            arrows: [],
+            nest: (row: 2, col: 2),
+            chickenStart: (row: 0, col: 0)
+        ),
+        
+        //Level 4
+        Level(
+            num: 4, rows: 3,
+            columns: 3,
+            flames: [(row: 0, col: 0), (row: 2, col: 1), (row: 2, col: 2)],
+            arrows: [],
+            nest: (row: 2, col: 0),
+            chickenStart: (row: 0, col: 1)
+        ),
+        
+        //Level 5
+        Level(
+            num: 5, rows: 3,
+            columns: 5,
+            flames: [(row: 0, col: 1), (row: 0, col: 2), (row: 0, col: 3), (row: 1, col: 1), (row: 1, col: 2), (row: 1, col: 3)],
+            arrows: [],
+            nest: (row: 0, col: 0),
+            chickenStart: (row: 0, col: 4)
+        ),
+        
+        //Level 6
+        Level(
+            num: 6, rows: 3,
+            columns: 5,
+            flames: [(row: 2, col: 0), (row: 2, col: 1), (row: 1, col: 1), (row: 0, col: 3), (row: 1, col: 3)],
+            arrows: [],
+            nest: (row: 0, col: 4),
+            chickenStart: (row: 1, col: 0)
+        ),
+        
+        //Level 7
+        Level(
+            num: 7, rows: 3,
+            columns: 5,
+            flames: [(row: 0, col: 0), (row: 0, col: 1), (row: 1, col: 1), (row: 2, col: 3), (row: 0, col: 4)],
+            arrows: [],
+            nest: (row: 2, col: 4),
+            chickenStart: (row: 1, col: 0)
+        ),
+        
+        //Level 8
+        Level(
+            num: 8, rows: 3,
+            columns: 4,
+            flames: [(row: 1, col: 0), (row: 1, col: 1), (row: 1, col: 2), (row: 0, col: 2), (row: 2, col: 2)],
+            arrows: [((row: 0, col: 1), .right), ((row: 2, col: 3), .left)],
+            nest: (row: 2, col: 0),
+            chickenStart: (row: 0, col: 0)
+        ),
+        
+        //Level 9
+        Level(
+            num: 9, rows: 4,
+            columns: 4,
+            flames: [(row: 0, col: 2), (row: 0, col: 3), (row: 2, col: 0), (row: 2, col: 2), (row: 2, col: 3), (row: 2, col: 3), (row: 2, col: 3), (row: 3, col: 1)],
+            arrows: [((row: 0, col: 1), .right), ((row: 1, col: 3), .down), ((row: 2, col: 1), .right), ((row: 3, col: 2), .left)],
+            nest: (row: 3, col: 0),
+            chickenStart: (row: 0, col: 0)
+        ),
+        
+        //Level 10
+        Level(
+            num: 10,
+            rows: 5,
+            columns: 4,
+            flames: [(row: 0, col: 2),(row: 0, col: 0), (row: 0, col: 3), (row: 2, col: 1), (row: 3, col: 2)],
+            arrows: [((row: 1, col: 0), .right), ((row: 2, col: 2), .left), ((row: 3, col: 0), .up), ((row: 3, col: 3), .up)],
+            nest: (row: 2, col: 0),
+            chickenStart: (row: 4, col: 0)
+        ),
+        
+        //Level 11
+        Level(
+            num: 11, rows: 4,
+            columns: 5,
+            flames: [(row: 1, col: 2),(row: 0, col: 1),(row: 0, col: 3), (row: 1, col: 4), (row: 2, col: 4), (row: 3, col: 4)],
+            arrows: [((row: 0, col: 2), .right), ((row: 0, col: 4), .down), ((row: 1, col: 3), .right), ((row: 2, col: 0), .right), ((row: 2, col: 1), .up), ((row: 2, col: 3), .down), ((row: 3, col: 1), .right), ((row: 3, col: 2), .left)],
+            nest: (row: 3, col: 0),
+            chickenStart: (row: 0, col: 0)
+        ),
+        
+        //Level 12
+        Level(
+            num: 12, rows: 3,
+            columns: 4,
+            flames: [(row: 1, col: 1),(row: 1, col: 2), (row: 1, col: 3), (row: 2, col: 0), (row: 2, col: 2)],
+            arrows: [((row: 0, col: 0), .right), ((row: 0, col: 3), .down), ((row: 2, col: 1), .up)],
+            nest: (row: 2, col: 3),
+            chickenStart: (row: 1, col: 0)
+        ),
+        
+    ]
+    
+    init() {
+        let passed = UserDefaults.standard.passedLevels
+        for i in 0..<levels.count {
+            if passed.contains(levels[i].num) {
+                levels[i].levelPass = true
+            }
+        }
+    }
+    
+    
+    func levelUp(index: Int) {
+        if index < levels.count {
+            levels[index].levelPass = true
+            
+            var passed = UserDefaults.standard.passedLevels
+            if !passed.contains(levels[index].num) {
+                passed.append(levels[index].num)
+                UserDefaults.standard.passedLevels = passed
+            }
+        }
+    }
+}
+
+extension UserDefaults {
+    private var passedLevelsKey: String { "passedLevelsKey" }
+    
+    var passedLevels: [Int] {
+        get {
+            return self.array(forKey: passedLevelsKey) as? [Int] ?? []
+        }
+        set {
+            self.set(newValue, forKey: passedLevelsKey)
+        }
+    }
+}
