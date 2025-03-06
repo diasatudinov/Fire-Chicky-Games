587b6b92ad5b6dd32abdba31234831d8cc13f107
Dias <dias.atudinov@gmail.com>
2025-03-06 19:22:13 +0500

    new files

---
+++ Fire Chicky Games/Fire Chicky Games/FCModules/FCGameView/FCTrapSweeperGameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FCTrapSweeperGameViewModel.swift
+//  Fire Chicky Games
+//
+//  Created by Dias Atudinov on 06.03.2025.
+//
+
+import Foundation
---
+++ Fire Chicky Games/Fire Chicky Games/FCModules/FCMenuView/FCMainView.swift
@@ -0,0 +1,18 @@
+//
+//  FCMainView.swift
+//  Fire Chicky Games
+//
+//  Created by Dias Atudinov on 06.03.2025.
+//
+
+import SwiftUI
+
+struct FCMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FCMainView()
+}
